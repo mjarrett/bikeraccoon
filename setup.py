154117feb3e6117791497af0ad324d53c8b4bcce
Mike Jarrett <msjarrett@gmail.com>
2021-04-25 16:45:39 -0700

    increment version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='bikeraccoon',
-      version='0.1',
+      version='0.2',
       description='Python utilities for accessing the raccoon.bike bikeshare API',
       author='Mike Jarrett',
       author_email='msjarrett@gmail.com',
