9ea6a4ef8f6fbe5cb81a9283e26a4b17d4a82f58
Mike Jarrett <msjarrett@gmail.com>
2025-05-24 15:17:18 -0700

    updates setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@
 from distutils.core import setup
 
 setup(name='bikeraccoon',
-      version='0.2',
+      version='1.0',
       description='Python utilities for accessing the raccoon.bike bikeshare API',
       author='Mike Jarrett',
       author_email='msjarrett@gmail.com',
       url='raccoon.bike',
       packages=['bikeraccoon'],
+      install_requires=['pandas','requires','duckdb','pyarrow','dash','flask'],
      )
